b73760d73ad111d6a586b6e8bbdf6554f8e93fbc
Othmar Ehrhardt <othmar.ehrhardt@t-online.de>
2021-01-07 23:26:02 +0100

    Add GPL header

--- TargetBridge/TargetBridge/Logger.cs
+++ TargetBridge/TargetBridge/Logger.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿/*
+ *  Copyright 2021, Othmar Ehrhardt <othmar.ehrhardt@t-online.de>, 
+ *                  https://astro.stroblhof-oberrohrbach.de
+ * 
+ *  This file is part of the TargetBridge project.
+ *
+ *  TargetBridge is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  TargetBridge is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License
+ *  along with TargetBridge.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
